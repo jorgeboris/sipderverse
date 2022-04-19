27af5f2a4b47365d225474625ebc3928bf642c39
Jorge Vargas <eduardo.borisq@gmail.com>
2022-04-19 17:27:22 -0500

    Prueba terminada

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,3 +1,5 @@
+const Spiderman = require("./../app/Spiderman")
+
 describe("Unit Tests for Spiderman class", () => {
     test('1) Create an spiderman object', () => {
 
@@ -7,7 +9,7 @@ describe("Unit Tests for Spiderman class", () => {
         expect(andrewGarfield.name).toBe("Spiderman Sony");
         expect(andrewGarfield.age).toBe(31);
         expect(andrewGarfield.actor).toBe("Andrew Garfield");
-        expect(andrewGarfield.movies).toBe(31);
+        expect(andrewGarfield.movies).toBe(2);
         expect(andrewGarfield.studio).toBe("Sony");
     });
   })
